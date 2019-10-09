85969363ad5a1bb03550a31283e065cc4357980a
ReymarEnteria <enteriarv@gmail.com>
2019-10-09 17:45:41 +0800

    25

--- app/regex.js
+++ app/regex.js
@@ -1,27 +1,50 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
-  containsNumber: function(str) {
+  containsNumber: function (str) {
+    let a = /[0-9]/g
+    return a.test(str)
+  },
+
+  containsRepeatingLetter: function (str) {
+    var hasDuplicates = (/([a-zA-Z]).*\1/).test(str)
+    return hasDuplicates
+  },
+
+  endsWithVowel: function (str) {
+    let vowel = /[aeiouAEIOU]\b/g
+    return vowel.test(str)
+  },
+
+  captureThreeNumbers: function (str) {
+    let vowel = /[^A-Za-z]{3}/g
+    if (str.match(vowel) === null) {
+      return false
+    } else {
+      return (str.match(vowel)[0])
+    }
+  },
+
+  matchesPattern: function (str) {
+    let three = /^\d{3}-\d{3}-\d{4}$/gm
+
+    if (str.match(three) === null) {
+      return false
+    } else {
+      return true
+    }
 
   },
 
-  containsRepeatingLetter: function(str) {
+  isUSD: function (str) {
+    let M = /^\d{3},\d{3},\d{3}.\d{2}$/gm
+    let T = /^\d{3},\d{3}.\d{2}$/gm
+    let H = /^\d{3}.\d{2}$/gm
 
-  },
-
-  endsWithVowel: function(str) {
-
-  },
-
-  captureThreeNumbers: function(str) {
-
-  },
-
-  matchesPattern: function(str) {
-
-  },
-
-  isUSD: function(str) {
-
+    if (str.match(three) === null) {
+      return false
+    } else {
+      return true
+    }
   }
 };
